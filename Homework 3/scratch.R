03195f7d195ee7e396d641415f67011ae9681d77
sudorook <daemon@nullcodon.com>
2019-04-18 21:13:51 -0500

    do hw 3

--- Homework 3/scratch.R
+++ Homework 3/scratch.R
@@ -1,31 +1,51 @@
 #! /usr/bin/env Rscript
 
-v <- c(0, 10, 4, 2, 5, 6, 10)
-w <- c(0, 3, 2, 6, 10, 20, 4)
+v <- c(10, 4, 2, 5, 6, 10)
+w <- c(3, 2, 6, 10, 20, 4)
 
-W <- sum(w)
-n <- length(v)
+W <- 15
 
-m <- matrix(0, ncol=(W+1), nrow=n)
-p <- matrix(0, ncol=(W+1), nrow=n)
+traceback <- function(P, w) {
+  i = nrow(P)
+  j = ncol(P)
 
-for (i in 2:(n)) {
-  for (j in 1:(W+1)) {
-    if (w[i] > j) {
-      m[i,j] <- m[i-1,j]
-      p[i,j] <- -1
+  items <- c()
+  while( (j > 0) && (i > 0) ) {
+    if (P[i,j] == 1) {
+      items <- append(items, i)
+      j = j - w[i]
+      i = i - 1
     } else {
-      m[i,j] <- max(m[i-1,j], m[i-1, j-w[i]] + v[i])
-      if (length(m[i-1, j-w[i]] + v[i]) == 0) {
-        p[i,j] <- -1
-      } else if (m[i-1,j] < (m[i-1, j-w[i]] + v[i])) {
-        p[i,j] <- 1
+      i = i - 1
+    }
+  }
+  return(items)
+}
+
+knapsackSearch <- function(v, w, W) {
+  n <- length(v)
+  m <- matrix(0, ncol=(W+1), nrow=(n+1))
+  p <- matrix(0, ncol=(W+1), nrow=(n+1))
+
+  for (i in 2:(n+1)) {
+    for (j in 2:(W+1)) {
+      if (w[i-1] > j-1) {
+        m[i,j] <- m[i-1,j]
+        p[i,j] <- 0
       } else {
-        p[i,j] <- -1
+        m[i,j] <- max(m[i-1,j], m[i-1, j-w[i-1]] + v[i-1])
+        if (m[i-1,j] < (m[i-1, j-w[i-1]] + v[i-1]))
+          p[i,j] <- 1
+        else
+          p[i,j] <- 0
       }
     }
   }
-}
+  
+  print((m[2:(n+1),2:(W+1)]))
+  print((p[2:(n+1),2:(W+1)]))
 
-t(m[2:(n),2:(W+1)])
-t(p[2:(n),2:(W+1)])
+  items <- traceback((p[2:(n+1),2:(W+1)]), w)
+  res <- list(score=m[n+1,W+1], items=items)
+  return(res)
+}
