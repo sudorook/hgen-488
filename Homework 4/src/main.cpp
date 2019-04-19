322b295d4cc4169c90340b578e0cde653571bbf5
sudorook <daemon@nullcodon.com>
2019-04-19 18:33:45 -0500

    minor cleanup; move seach printing to its own function

--- Homework 4/src/main.cpp
+++ Homework 4/src/main.cpp
@@ -20,6 +20,12 @@ struct global_hash_table {
   int count=0;
 };
 
+struct subsequence_match {
+  int id;
+  std::string sequence;
+  std::string match;
+};
+
 static std::map<char, int> dna = {
   { 'A', 0 },
   { 'C', 1 },
@@ -37,11 +43,11 @@ int dna_hash(std::string triplet) {
   return(code);
 }
 
-void print_global_hash_table(global_hash_table *table) {
+void print_global_hash_table(global_hash_table *hash_table) {
   for (int i=0; i!=CODON_TABLE_SIZE; i++) {
-    if (table->table[i].size() > 0) {
+    if (hash_table->table[i].size() > 0) {
       std::cout << i << ": ";
-      for (auto j = table->table[i].begin(); j != table->table[i].end(); j++) {
+      for (auto j = hash_table->table[i].begin(); j != hash_table->table[i].end(); j++) {
         std::cout << "(" << std::get<0>(*j) << ',' << std::get<1>(*j) << ") ";
       }
       std::cout<<std::endl;
@@ -50,56 +56,43 @@ void print_global_hash_table(global_hash_table *table) {
   return;
 }
 
-void print_global_hash_table(std::array<std::vector<std::tuple<int,int>>, CODON_TABLE_SIZE> *table) {
-  for (int i=0; i!=CODON_TABLE_SIZE; i++) {
-    if ((*table)[i].size() > 0) {
-      std::cout << i << ": ";
-      for (auto j = (*table)[i].begin(); j != (*table)[i].end(); j++) {
-        std::cout << "(" << std::get<0>(*j) << ',' << std::get<1>(*j) << ") ";
-      }
-      std::cout<<std::endl;
-    }
-  }
-  return;
-}
-
-void append_global_hash_table(struct global_hash_table *table, std::string seq) {
+void append_global_hash_table(struct global_hash_table *hash_table, std::string seq) {
   int len = seq.size();
 
-  table->count = table->count+1;
-  table->sequences.push_back(seq);
+  hash_table->count = hash_table->count+1;
+  hash_table->sequences.push_back(seq);
 
   for (int i=0; i!=len-2; i++) {
     std::string substring = seq.substr(i, 3);
-    table->table[dna_hash(substring)].push_back(std::tuple<int,int>(table->count,i));
+    hash_table->table[dna_hash(substring)].push_back(std::tuple<int,int>(hash_table->count,i));
   }
   return;
 }
 
-void substring_search(struct global_hash_table *table, std::string substring) {
+std::vector<struct subsequence_match> substring_search(struct global_hash_table *hash_table, std::string substring) {
   int len = substring.size();
   std::vector<std::vector<std::tuple<int,int,int>>> search_table;
-  
+
   // Subset the global alignment by the motifs present in the sequence being
   // searched, and create a table of triplets (seq id, position of start of
   // sequence, position of start of motif).
   for (int i=0; i!=len-2; i++) {
     std::string s = substring.substr(i, 3);
     std::vector<std::tuple<int,int,int>> v;
-    for (auto j = table->table[dna_hash(s)].begin(); j != table->table[dna_hash(s)].end(); j++) {
+    for (auto j = hash_table->table[dna_hash(s)].begin(); j != hash_table->table[dna_hash(s)].end(); j++) {
       v.push_back(std::tuple<int,int,int>(std::get<0>(*j), (std::get<1>(*j))-i, std::get<1>(*j)));
     }
     search_table.push_back(v);
 
-    // print the output
-    std::cout << i << ": ";
-    for (auto j = v.begin(); j != v.end(); j++) {
-      std::cout << "(" << std::get<0>(*j) << ','<< std::get<1>(*j) << ',' << std::get<2>(*j) << ") ";
-    }
-    std::cout<<std::endl;
+    /* // print the output
+     * std::cout << i << ": ";
+     * for (auto j = v.begin(); j != v.end(); j++) {
+     *   std::cout << "(" << std::get<0>(*j) << ','<< std::get<1>(*j) << ',' << std::get<2>(*j) << ") ";
+     * }
+     * std::cout<<std::endl; */
   }
 
-  std::vector<std::tuple<int,int>> indices;
+  std::vector<struct subsequence_match> matches;
 
   for (int i=0; i<(int)search_table[0].size(); i++) {
     bool flag = true;
@@ -119,23 +112,25 @@ void substring_search(struct global_hash_table *table, std::string substring) {
     }
 
     if (flag == true) {
-      indices.push_back(std::tuple<int,int>(id,start));
+      struct subsequence_match match;
+      match.sequence = hash_table->sequences[id-1];
+      match.id = id;
+      match.match = std::string(start,'.') + substring +
+        std::string(match.sequence.size() - substring.size() - start,'.');
+      matches.push_back(match);
     }
   }
 
-  for (auto j = indices.begin(); j != indices.end(); j++) {
-    int id = std::get<0>(*j);
-    int start = std::get<1>(*j);
-    std::string sequence = table->sequences[std::get<0>(*j)-1];
+  return(matches);
+}
 
-    std::cout<<"Sequence "<<id<<": "<<std::endl;
-    std::cout<<sequence<<std::endl;
-    std::cout<<std::string(start,'.');
-    std::cout<<substring;
-    std::cout<<std::string(sequence.size() - substring.size() - start,'.')<<std::endl;
+void print_substring_matches(std::vector<struct subsequence_match> matches) {
+  for (auto match = matches.begin(); match != matches.end(); match++) {
+    std::cout<<"Sequence "<<match->id<<": "<<std::endl;
+    std::cout<<match->sequence<<std::endl;
+    std::cout<<match->match<<std::endl;
+    std::cout<<std::endl;
   }
-
-  return;
 }
 
 int main(void) {
@@ -145,9 +140,10 @@ int main(void) {
   append_global_hash_table(&table, S2);
   append_global_hash_table(&table, S3);
   append_global_hash_table(&table, S4);
-  print_global_hash_table(&table);
+  // print_global_hash_table(&table);
 
-  substring_search(&table, S);
+  std::vector<struct subsequence_match> matches = substring_search(&table, S);
+  print_substring_matches(matches);
 
   return(0);
 }
