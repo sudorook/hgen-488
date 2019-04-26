bc4460cf4451db53c694408215f40cd0f4ca2cf9
sudorook <daemon@nullcodon.com>
2019-04-25 22:30:22 -0500

    cleanup commented code

--- Homework 4/bwt/src/main.cpp
+++ Homework 4/bwt/src/main.cpp
@@ -4,10 +4,7 @@
 #include <map>
 #include <algorithm>
 
-// static const std::string string = "ABAABA";
-// static const std::string string = "BANANA";
-static const std::string string = "DOGWOOD";
-// static const std::string string = "DOGWORD";
+static const std::string main_string = "DOGWORD";
 
 struct LF_map {
   std::vector<std::tuple<char, int>> F;
@@ -57,7 +54,7 @@ void print_LF_map(struct LF_map map) {
 }
 
 void print_LF_map(std::string bwt_string) {
-  struct LF_map map = make_LF_map(string);
+  struct LF_map map = make_LF_map(main_string);
   std::cout<<"  F   ...   L  "<<std::endl;
   for (int i = 0; i<(int)map.F.size(); i++) {
     std::cout<<"("<<std::get<0>(map.F[i])<<","<<std::get<1>(map.F[i])<<")"<<" ... ";
@@ -135,29 +132,22 @@ std::string inverse_BWT(std::string bwt_string) {
 
 std::vector<std::string> substring_search(LF_map map, std::string substring) {
   std::vector<std::vector<int>> indices;
-  // std::cout<<substring<<std::endl;
 
   // Get rows in the LF map that match the end of the query string.
   for (int j=0; j<(int)map.F.size(); j++) {
     int end = substring.size()-1;
     if (std::get<0>(map.F[j]) == substring[end]) {
       indices.push_back(std::vector<int>{j});
-      // std::cout<<substring[end]<<": "<<j<<std::endl;
     }
   }
 
   int counter = (int)substring.size()-2;
   while ((indices.size() != 0) && counter >= 0) {
-  /* while ((indices.size() != 0) && counter < (int)substring.size()) { */
     for (int i=0; i<(int)indices.size(); i++) {
       std::vector<int> *index = &indices[i];
       int idx = (*index)[index->size()-1];
 
-      // std::cout<<"idx: "<<idx<<std::endl;
-      // std::cout<<"new: "<<std::get<0>(map.L[idx])<<std::endl;
-
       if (std::get<0>(map.L[idx]) == substring[counter]) {
-        // std::cout<<"yay: "<<substring[counter]<<std::endl;
         int new_idx = std::distance(map.F.begin(), std::find(map.F.begin(),
               map.F.end(), map.L[idx]));
         index->push_back(new_idx);
@@ -169,43 +159,18 @@ std::vector<std::string> substring_search(LF_map map, std::string substring) {
       }
     }
     counter--;
-
-    /* std::cout<<"counter:    "<<counter<<std::endl;
-     * std::cout<<"index size: "<<indices.size()<<std::endl;
-     * for (int i=0; i < (int)indices.size(); i++) {
-     *   std::cout<<"index "<<i<<" size: "<<indices[i].size()<<std::endl;
-     *   std::cout<<"   ";
-     *   for (int j=0; j < (int)indices[i].size(); j++) {
-     *     std::cout<<indices[i][j]<<" ";
-     *   }
-     *   std::cout<<std::endl;
-     * } */
   }
 
   char c;
-  // std::cout<<"n matches: "<<indices.size()<<std::endl;
-  // std::string orig_sequence = inverse_BWT(map);
   std::vector<std::string> results;
   for (int i=0; i < (int)indices.size(); i++) {
-    /* match.empty(); */
     std::string match;
     for (int j=0; j < (int)substring.size(); j++) {
-      // std::cout<<std::get<0>(map.F[indices[i][j]]);
       c = std::get<0>(map.F[indices[i][j]]);
       match = c + match;
     }
 
     results.push_back(match);
-
-    // // std::string prefix = std::string(indices[i][indices[i].size()-1]-1, '.');
-    // std::string prefix = std::string(indices[i][indices[i].size()-1], '.');
-    // std::cout<<indices[i][indices[i].size()-1]<<std::endl;
-    // match = prefix + match + std::string(orig_sequence.size() - prefix.size() - match.size(), '.');
-    // [> match = prefix + match; <]
-    // 
-    // std::cout<<orig_sequence<<std::endl;
-    // std::cout<<match<<std::endl;
-    // std::cout<<std::endl;
   }
   return results;
 }
@@ -220,8 +185,8 @@ int main(void) {
 
   // Obtain BWT string for "DOGWORD"
   std::cout<<"BW Transform:"<<std::endl;
-  std::string bwt_string = BWT(string);
-  std::cout<<"orig: "<<string<<std::endl;
+  std::string bwt_string = BWT(main_string);
+  std::cout<<"orig: "<<main_string<<std::endl;
   std::cout<<"bwt:  "<<bwt_string<<std::endl;
   std::cout<<std::endl;
 
@@ -232,15 +197,13 @@ int main(void) {
   // Obtain "DOGWORD" from the LF mapping of BWT
   std::cout<<"Inverse BWT by LF mapping:"<<std::endl;
   std::string lfmap_string = inverse_BWT(bwt_string);
-  std::cout<<"orig:    "<<string<<std::endl;
+  std::cout<<"orig:    "<<main_string<<std::endl;
   std::cout<<"bwt:     "<<bwt_string<<std::endl;
   std::cout<<"inv bwt: "<<lfmap_string<<std::endl; 
   std::cout<<std::endl;
 
   // Search substring via LF map
   std::cout<<"Search for substring 'GWO'"<<std::endl;
-  // substring_search(map, "GWO");
-  // std::vector<std::string> results = substring_search(map, "ANA");
   std::vector<std::string> results = substring_search(map, "GWO");
   print_substrings(results);
 
