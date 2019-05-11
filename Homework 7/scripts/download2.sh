526d656b6a1f3bf1c85c49163ec5287aa15d6036
sudorook <daemon@nullcodon.com>
2019-05-11 18:26:01 -0500

    do hw7

---
+++ Homework 7/scripts/download2.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+set -eu
+
+urlprefix="http://gdac.broadinstitute.org/runs/stddata__2016_01_28/data/HNSC/20160128/"
+archive="gdac.broadinstitute.org_HNSC.Merge_rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.Level_3.2016012800.0.0.tar.gz"
+dirname="gdac.broadinstitute.org_HNSC.Merge_rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.Level_3.2016012800.0.0"
+filename="HNSC.rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.data.txt"
+
+mkdir -p ../raw_data
+cd ../raw_data
+
+wget -nc "${urlprefix}${archive}"
+tar xf "${archive}"
+
+mkdir -p ../data
+mv "${dirname}/${filename}" ../data/data.txt
