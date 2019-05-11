1973814c3c2f9a2824e4bf46c8babb0aa15b7140
sudorook <daemon@nullcodon.com>
2019-05-11 16:41:13 -0500

    wrong dataset

--- Homework 7/scripts/download.sh
+++ Homework 7/scripts/download.sh
@@ -4,30 +4,29 @@ set -eu
 sigfile="../data/sig_genes.txt"
 allfile="../data/all_genes.txt"
 
-rm -f "${sigfile}"
-rm -f "${allfile}"
-
 urlprefix="http://gdac.broadinstitute.org/runs/analyses__2016_01_28/reports/cancer/SKCM-TM/Pathway_GSEA_mRNAseq/"
 prefix="SKCM-TM-mRNAseq_cNMF-clus"
 suffix="___Class2-CanonicalPathway"
 
+mkdir -p ../data
+rm -f "${sigfile}"
+rm -f "${allfile}"
+
+echo -e "gene.id\tp.val\tclass" >> "${sigfile}"
+echo -e "gene.id\tlog.fold.change\tfold.change\tclass" >> "${allfile}"
+
 mkdir -p ../raw_data/
 cd ../raw_data/
+
 for i in $(seq 1 4); do
   wget -nc "${urlprefix}${prefix}${i}${suffix}.significant.genes.txt"
   wget -nc "${urlprefix}${prefix}${i}${suffix}.up.regulated.genes.txt"
   wget -nc "${urlprefix}${prefix}${i}${suffix}.down.regulated.genes.txt"
 
-  if [[ ${i} = 1 ]]; then
-    cat "${prefix}${i}${suffix}.significant.genes.txt" >> ${sigfile}
-    cat "${prefix}${i}${suffix}.up.regulated.genes.txt" >> ${allfile}
-    cat "${prefix}${i}${suffix}.down.regulated.genes.txt" >> ${allfile}
-  else
-    cat "${prefix}${i}${suffix}.significant.genes.txt" | \
-      tail -n +2 >> ${sigfile}
-    cat "${prefix}${i}${suffix}.up.regulated.genes.txt" | \
-      tail -n +2 >> ${allfile}
-    cat "${prefix}${i}${suffix}.down.regulated.genes.txt" | \
-      tail -n +2 >> ${allfile}
-  fi
+  cat "${prefix}${i}${suffix}.significant.genes.txt" | sed -e "s/$/\t${i}/" | \
+    tail -n +2 >> "${sigfile}"
+  cat "${prefix}${i}${suffix}.up.regulated.genes.txt" | sed -e "s/$/\t${i}/" | \
+    tail -n +2 >> "${allfile}"
+  cat "${prefix}${i}${suffix}.down.regulated.genes.txt" | sed -e "s/$/\t${i}/" | \
+    tail -n +2 >> "${allfile}"
 done
