839262b51c78f2de869e4e1388f5ac7bdc19010d
sudorook <daemon@nullcodon.com>
2019-06-03 22:38:02 -0500

    add some nonsense to the scratch file

--- Presentation/scratch.R
+++ Presentation/scratch.R
@@ -1,5 +1,64 @@
 set.seed(10)
 
+# Biclustering demonstration
+library(biclust)
+
+# n <- 20
+# m <- 20
+
+# mus <- c(-1, 0, 1)
+# sds <- c(.1, .1, .1)
+# 
+# X <- matrix(0, nrow=m, ncol=n)
+# for (i in 1:m) {
+#   for (j in 1:n) {
+#     id <- ceiling(runif(1)*4)
+#     if ( id <= length(mus) ) {
+#       X[i,j] <- rnorm(1, mus[id], sds[id])
+#     }
+#   }
+# }
+
+# X <- matrix(rbinom(n*m, 50, c(.6, .4, .2)), nrow=m, ncol=n)
+# # res <- biclust(X, method=BCXmotifs(), alpha=0.05, number=50)
+# res <- biclust(X, method=BCCC(), alpha=0.05, number=50)
+# 
+# heatmap(x=X, Rowv=NA, Colv=NA, reorderfun=NA)
+# heatmapBC(x=X, bicResult=res, xlab=NA, ylab=NA)
+
+
+data(BicatYeast)
+X <- discretize(BicatYeast)
+# X <- BicatYeast
+
+X.df <- data.frame(X)
+res <- biclust(X, method=BCXmotifs(), alpha=0.05, number=50)
+# res <- biclust(X, method=BCCC(), delta=1.5, alpha=0.05, number=50)
+res <- biclust(X, method=BCCC(), alpha=0.05, number=10)
+# res2 <- biclust(X, method=BCCC())
+
+png('heatmap.png')
+heatmap(x=X, Rowv=NA, Colv=NA, reorderfun=NA)
+dev.off()
+
+png('heatmapBC.png')
+heatmapBC(x=X, bicResult=res, xlab=NA, ylab=NA)
+dev.off()
+
+
+# Sparse SVD
+
+library('ssvd')
+
+# X <- matrix(rnorm(2^15), 2^7, 2^8)
+X <- matrix(rnorm(100), 10, 10)
+X[sample(1:length(X), 10)] <- rnorm(10, 5, 2)
+res <- ssvd(X, method = "method")
+ans.initial <- ssvd.initial(X, method = "method")
+ans.iter <- ssvd.iter.thresh(X, u.old=ans.initial$u, v.old=ans.initial$v, method = "method")
+
+#
+
 n <- 10
 m <- 10
 ct <- .25
